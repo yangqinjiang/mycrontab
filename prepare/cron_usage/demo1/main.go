925a1554c0360192ed103494eec21061495066aa
yangqinjiang <yangqinjiang@qq.com>
2018-12-09 21:20:24 +0800

    cron使用

---
+++ prepare/cron_usage/demo1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gorhill/cronexpr"
+	"fmt"
+	"time"
+)
+
+func main() {
+	//每5s执行一次
+		expr , err :=cronexpr.Parse("*/5 * * * * * *")
+		if err != nil{
+			fmt.Println(err)
+		}
+		//  /5
+		// 0 5 10 15
+		//当前时间
+		now := time.Now()
+		//下次调度时间
+		nextTime := expr.Next(now)
+		fmt.Println("下次调度时间:",nextTime)
+
+		//等待这个定时器超时
+		time.AfterFunc(nextTime.Sub(now), func() {
+			fmt.Println("被调度了",time.Now())
+		})
+		time.Sleep(5 * time.Second)
+		fmt.Println("主协程退出",time.Now())
+
+}

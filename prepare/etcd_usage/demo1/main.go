dbab5c3de6ac709e50b505578802dde1533009d6
yangqinjiang <531553280@qq.com>
2018-12-11 13:25:08 +0800

    etcd的使用

---
+++ prepare/etcd_usage/demo1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+	"context"
+)
+
+func main() {
+	//在服务器启动etcd
+	// nohup ./etcd --listen-client-urls 'http://0.0.0.0:2379' --advertise-client-urls 'http://0.0.0.0:2379' &
+	config:=clientv3.Config{
+		Endpoints:[]string{"127.0.0.1:2379"},//集群列表
+		DialTimeout:5*time.Second,
+	}
+
+	//建立一个客户端
+	client,err := clientv3.New(config)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("链接成功")
+	//用于读写etcd的键值对
+	kv:=clientv3.NewKV(client)
+	putResp,err := kv.Put(context.TODO(),"/cron/jobs/job1","hi0",clientv3.WithPrevKV())
+	if err != nil{
+		fmt.Println(err)
+		return
+	}else{
+		fmt.Println("Revision:",putResp.Header.Revision)
+
+		if putResp.PrevKv !=nil{//put的时候,必须 clientv3.WithPrevKV()
+			fmt.Println("PrevKv",string(putResp.PrevKv.Value))
+		}
+	}
+
+
+
+
+}

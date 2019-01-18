d143e3a4b6ce69e467de43f175246cf52cb54dca
yangqinjiang <531553280@qq.com>
2019-01-18 10:59:25 +0800

    任务的执行器 的接口

--- crontab/worker/Scheduler_test.go
+++ crontab/worker/Scheduler_test.go
@@ -1,6 +1,7 @@
 package worker
 
 import (
+	"errors"
 	"testing"
 	"github.com/yangqinjiang/mycrontab/crontab/common"
 	"time"
@@ -57,22 +58,29 @@ func TestScheduler_PushJobResult(t *testing.T) {
 		t.Fatal("PushJobResult 失败,数量== 3")
 	}
 }
-
-type TestJobExec struct {
+//执行任务成功的
+type TestJobExecSuccess struct {
 }
 
-func (je *TestJobExec)Exec(info *common.JobExecuteInfo)(err error)  {
+func (je *TestJobExecSuccess)Exec(info *common.JobExecuteInfo)(err error)  {
 	return
+}
+//执行任务失败的
+type TestJobExecFail struct {
+}
+
+func (je *TestJobExecFail)Exec(info *common.JobExecuteInfo)(err error)  {
+	return errors.New("执行任务失败")
 }
 
 //尝试执行任务
-func TestScheduler_TryStartJob(t *testing.T) {
+func TestScheduler_TryStartJobSuccess(t *testing.T) {
 	//第一次初始化任务调度器
 	err,_ := InitScheduler(nil)
 	if err != nil {
 		t.Fatal("初始化任务调度器 失败",err)
 	}
-	G_scheduler.SetJobExecuter(&TestJobExec{})
+	G_scheduler.SetJobExecuter(&TestJobExecSuccess{})
 
 	//FAIL,cron表达式是错误的,
 	job_fail := &common.Job{Name: "TryStartJob",CronExpr:"error cron",Command:"echo hello"}
@@ -92,6 +100,29 @@ func TestScheduler_TryStartJob(t *testing.T) {
 	}
 	err = G_scheduler.TryStartJob(jobSchedulePlan)
 	if err != nil{
-		t.Fatal("执行任务出错")
+		t.Fatal("执行任务应该是正确的",err)
+	}
+}
+
+
+//尝试执行任务
+func TestScheduler_TryStartJobFail(t *testing.T) {
+	//第一次初始化任务调度器
+	err,_ := InitScheduler(nil)
+	if err != nil {
+		t.Fatal("初始化任务调度器 失败",err)
+	}
+	G_scheduler.SetJobExecuter(&TestJobExecFail{})
+	//cron表达式是正确的
+	job := &common.Job{Name: "TryStartJob",CronExpr:"* * * * * *",Command:"echo hello"}
+	jobEvent := common.BuildJobEvent(common.JOB_EVENT_KILL, job)
+	var jobSchedulePlan *common.JobSchedulePlan
+	if jobSchedulePlan, err = common.BuildJobSchedulePlan(jobEvent.Job); err != nil {
+		t.Fatal("构建任务调度计划失败",err)
+		return
+	}
+	err = G_scheduler.TryStartJob(jobSchedulePlan)
+	if err == nil{
+		t.Fatal("执行任务应该是出错的",err)
 	}
 }
